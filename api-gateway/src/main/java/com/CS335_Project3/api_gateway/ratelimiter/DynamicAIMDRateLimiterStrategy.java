90babea6840f033061922154cdc69258cb4ccd4d
Vlad <vladmysiv@gmail.com>
2026-04-16 21:00:42 +0100

    Addet adaptive rate liming algorithm (AIMD)

--- api-gateway/src/main/java/com/CS335_Project3/api_gateway/ratelimiter/DynamicAIMDRateLimiterStrategy.java
+++ api-gateway/src/main/java/com/CS335_Project3/api_gateway/ratelimiter/DynamicAIMDRateLimiterStrategy.java
@@ -19,11 +19,11 @@ public class DynamicAIMDRateLimiterStrategy implements RateLimiterStrategy {
 
     //Max value for latency before reducing the limit
     @Value("${rate-limit.aimd.latency-threshold-ms:500}")
-    private long latencyThresholdMs;
+    private long latencyThreshold;
 
     //increase window by additive 1
     @Value("${rate-limit.aimd.additive-increase:1}")
-    private int additiveIncrease;
+    private int additiveFactor;
 
     //reduce window by multiplicate 2
     @Value("${rate-limit.aimd.decrease-factor:0.5}")
@@ -56,9 +56,8 @@ public class DynamicAIMDRateLimiterStrategy implements RateLimiterStrategy {
         return baseLimiter.isRequestAllowed(stateKey, currentLimit);
     }
 
-    /*
-        The Controller Loop: Runs every 10 seconds to adjust limits based on latency.
-    */
+
+    //The Controller Loop: Runs every 10 seconds to adjust limits based on latency.
     @Scheduled(fixedRateString = "${rate-limit.aimd.evaluation-period-ms:10000}")
     public void evaluateAndAdjustLimits() {
         // Get all clients currently making requests
@@ -66,7 +65,7 @@ public class DynamicAIMDRateLimiterStrategy implements RateLimiterStrategy {
 
         for (String clientId : activeClients) {
             Map<String, Long> percentiles = metricsService.getLatencyPercentiles(clientId);
-            long p95Latency = percentiles.getOrDefault("p50", 0L);
+            long p95Latency = percentiles.getOrDefault("p50", 0L); //set p50 for simplicity, but in real case, use p95
 
             String limitKey = KEY_PREFIX + clientId;
             String currentLimitStr = redisTemplate.opsForValue().get(limitKey);
@@ -78,12 +77,12 @@ public class DynamicAIMDRateLimiterStrategy implements RateLimiterStrategy {
             int currentLimit = (currentLimitStr != null) ? Integer.parseInt(currentLimitStr) : maxAllowedLimit;
             int newLimit = currentLimit;
 
-            if (p95Latency > latencyThresholdMs) {
+            if (p95Latency > latencyThreshold) {
                 // Backend is congested -> Multiplicative Decrease
                 newLimit = Math.max(1, (int) (currentLimit * decreaseFactor));
             } else if (p95Latency > 0) {
                 // Backend is healthy -> Additive Increase
-                newLimit = Math.min(maxAllowedLimit, currentLimit + additiveIncrease);
+                newLimit = Math.min(maxAllowedLimit, currentLimit + additiveFactor);
             }
 
             // Save the newly calculated limit to Redis
