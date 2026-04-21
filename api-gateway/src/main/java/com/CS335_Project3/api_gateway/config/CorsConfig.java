4459ef173a42b44088f0611adbda2233fe99fc6f
Cathy0326 <cathyyue0326@gmail.com>
2026-04-21 16:41:56 +0100

    add CORS Headers tell browser dashboard port 3000 is allowed

---
+++ api-gateway/src/main/java/com/CS335_Project3/api_gateway/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.CS335_Project3.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    @Order(-1)
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
